a4baa557125ab98c50e25396081d3dfb4aa01a13
steview2000 <steview2000@openmailbox.org>
2021-02-19 20:59:35 +0100

    Simplify analysis.sh

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-
-
+"/dev/fd/63" line 45: warning: No valid data points found in file
+"/dev/fd/63" line 46: undefined variable: STATS_min_y
 
 # COVID-19-plots
 
@@ -7,7 +7,7 @@ Here is some regularly updated analysis of the COVID-19 outbreak.
 
 Data from: https://github.com/CSSEGISandData/COVID-19
 
- *UPDATED: Sun 17 May 2020 02:28:11 PM CEST*
+ *UPDATED: Thu 18 Feb 2021 10:56:06 PM CET*
 
 Data are provided for number of the number of _cumulated infected_, _recovered_ and _deceased_ patients.
 
@@ -18,7 +18,7 @@ deaths and recovered patients. This number is certainly important for the health
 normalise them by the population. Not that this normalisation is still not sufficient, e.g., when
 the infections mainly occur in a rather small area of a countr. We plot the number of normalised by the population of ad given country (in person per millions - ppm).
 
-![Currently sick](plots/curr-sick.png)
+![Currently sick](curr-sick.png)
 
 
 ## The growth rate of infection
@@ -27,24 +27,43 @@ We first consider the number of infected people. With a constant infection rate 
 reservoir of _infectable_ people. This number should grow initially nearly exponentially without
 proper countermeasures. We plot the data on a linear x-axis and a logarithmic y-axis. In this way an exponential growth of the infections shows up as straight lines. 
 
-![Exponential development](plots/poly-exp-fit.png)
+![Exponential development](poly-exp-fit.png)
 
 We see in the top figure, the infection data for 6 different countries. Most of them do not show
 straight lines over the entire range. We calculate the relative infection rate from smoothed and
 interpolated infection data in order to remove the scatter of the data. 
 
-*Infection rates doubled in :*
-|Country | days until doubling of infections| 
-| --- | --- |
-|germany |208.13 days|
-|italy |181.06 days|
-|spain |272.38 days|
-|france |718.98 days|
-|sweden |37.88 days|
-|us |39.68 days|
+_doubling_table
 
+## Logisitc function
+The number of infected agents during a pandemic often follow a [logistic function](https://en.wikipedia.org/wiki/Logistic_function).
+The number of infected agents increases initially exponentially, as long as most of the population
+is not infected yet. When the infection rate increases, it will be more difficult for the virus to
+find fresh (not infected) hosts and the slope of the curve decreases. The curve reaches its
+inflection point when roughly half of the population is infected and after finally reaches
+asymptotically a constant value, i.e., uninfected hosts are so rare that the virus can no longer
+spread. 
 
-*Please note that I have removed the logistic fits, as they are at this point of the pandemic not
-very useful. The logistic curve only models the pandemic well when no measures
-are taken. The measures that were taken and the change of the restrictions makes a good long term
-projection impossible and we therefore refrain from doing so.*
+The COVID-19 pandemic is however different, as the infection rates are not so much controlled by the
+progress of the infections, but rather by the measures the societies are taken to slow down the
+outbreak. Nevertheless, fitting a logisitic function to the data might help to detect changes in the
+spreading rate and hence whether measures are working.
+
+![Logistic Curve](logistic-curve.png)
+
+Top: Data with logistic curve fits. 
+Bottom: The derivative of the logistic curve (i.e., the daily increases) as lines, and the real
+increases as data points.
+
+_logistic_table
+
+## Another fit
+
+As pointed out, the logisitc function represents the data well only, when no interventions are
+taken place, or under a complete shutdown. However, it is likely that certain societies decide to
+accept a slow, constant increase of cases, small enough for the health systems to deal with. Many
+countries, including South Korea show a slow linear increase of the accumulated infections.
+Therefore, we also try a fit of the function: I(t) = (a+b*t)/(1+exp(-c*t)) 
+
+![Logist-linear](logistic-linear.png)
+
--- analysis.sh
+++ analysis.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Script to analyse the corona epidemics
 
+CURRENT_PATH=$(pwd)
+
+echo "First get the data ...."
+cd $HOME/COVID-19
+#git pull
+cd $CURRENT_PATH
+
 export COUNTRY1=germany
 export COUNTRY2=italy
 export COUNTRY3=spain
@@ -12,103 +19,18 @@ export CONFIRMED="$HOME/COVID-19/csse_covid_19_data/csse_covid_19_time_series/ti
 export DEATH="$HOME/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 export RECOVERED="$HOME/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
-COL_Number=$(csvtool col 122 <(csvtool transpose $CONFIRMED) |tail -n +5 |wc -l)
+#COL_Number=$(csvtool col 122 <(csvtool transpose $CONFIRMED) |tail -n +5 |wc -l)
 
 function create_confirmed {
-	case $1 in
-		germany) 
-			csvtool col 1,122 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		italy) 
-			csvtool col 1,139 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		us) 
-			csvtool col 1,227 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		uk) 
-			csvtool col 1,225 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		spain) 
-			csvtool col 1,203 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		korea) 
-			csvtool col 1,145 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		sweden) 
-			csvtool col 1,207 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		france) 
-			csvtool col 1,118 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-		malaysia) 
-			csvtool col 1,155 <(csvtool transpose $CONFIRMED) |tail -n +5 
-			;;
-	esac
+	grep -i $1 $CONFIRMED |tr "," "\n" |tail -n +5 
 }
 
 function create_recovered {
-	case $1 in
-		germany) 
-			csvtool col 114 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		italy) 
-			csvtool col 133 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		us) 
-			csvtool col 227 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		uk) 
-			csvtool col 225 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		spain) 
-			csvtool col 201 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		korea) 
-			csvtool col 139 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		sweden) 
-			csvtool col 205 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		france) 
-			csvtool col 110 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-		malaysia) 
-			csvtool col 150 <(csvtool transpose $RECOVERED) |tail -n +5 
-			;;
-	esac
-
+	grep -i $1 $RECOVERED |tr "," "\n" |tail -n +5 
 }
 
 function create_deaths {
-	case $1 in
-		germany) 
-			csvtool col 122 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		italy) 
-			csvtool col 139 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		us) 
-			csvtool col 227 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		uk) 
-			csvtool col 225 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		spain) 
-			csvtool col 203 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		korea) 
-			csvtool col 145 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		sweden) 
-			csvtool col 207 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		malaysia) 
-			csvtool col 155 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-		france) 
-			csvtool col 118 <(csvtool transpose $DEATH) |tail -n +5 
-			;;
-	esac
+	grep -i $1 $DEATH |tr "," "\n" |tail -n +5 
 }
 
 function create_all_col {
@@ -118,16 +40,16 @@ function create_all_col {
 
 #create_recovered korea
 
-create_all_col germany 1 | awk -F, '{print $1,$2,$3,$4,83}'> data/germany.csv
-create_all_col italy 1 | awk -F, '{print $1,$2,$3,$4,60}' > data/italy.csv
-create_all_col france 1 | awk -F, '{print $1,$2,$3,$4,67}'> data/france.csv
-create_all_col spain 1 | awk -F, '{print $1,$2,$3,$4,47}'> data/spain.csv
-create_all_col korea 2 | awk -F, '{print $1,$2,$3,$4,77}'> data/korea.csv
-create_all_col malaysia 1 | awk -F, '{print $1,$2,$3,$4,33}'> data/malaysia.csv
-create_all_col iran 1 | awk -F, '{print $1,$2,$3,$4,83}'> data/iran.csv
-create_all_col uk df 1 | awk -F, '{print $1,$2,$3,$4,67}'> data/uk.csv
-create_all_col us 1 | awk -F, '{print $1,$2,$3,$4,328}'> data/us.csv
-create_all_col sweden 1 | awk -F, '{print $1,$2,$3,$4,10}'> data/sweden.csv
+create_all_col germany 1  | awk -F, '{print $1,$2,$3,$4,83}'  > data/germany.csv
+create_all_col italy 1    | awk -F, '{print $1,$2,$3,$4,60}'  > data/italy.csv
+create_all_col france 1   | awk -F, '{print $1,$2,$3,$4,67}'  > data/france.csv
+create_all_col spain 1    | awk -F, '{print $1,$2,$3,$4,47}'  > data/spain.csv
+create_all_col korea 2    | awk -F, '{print $1,$2,$3,$4,77}'  > data/korea.csv
+create_all_col malaysia 1 | awk -F, '{print $1,$2,$3,$4,33}'  > data/malaysia.csv
+create_all_col iran 1     | awk -F, '{print $1,$2,$3,$4,83}'  > data/iran.csv
+create_all_col uk df 1    | awk -F, '{print $1,$2,$3,$4,67}'  > data/uk.csv
+create_all_col us 1       | awk -F, '{print $1,$2,$3,$4,328}' > data/us.csv
+create_all_col sweden 1   | awk -F, '{print $1,$2,$3,$4,10}'  > data/sweden.csv
 
 gnuplot -p <(sed -e "s/country1/$COUNTRY1/g" -e "s/country2/$COUNTRY2/g" -e "s/country3/$COUNTRY3/g" -e "s/country4/$COUNTRY4/g" -e "s/country5/$COUNTRY5/g" -e "s/country6/$COUNTRY6/g" plot.gpl) 2>gnuout.txt
 ##
